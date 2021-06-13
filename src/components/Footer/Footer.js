13567bf410adfd2da39e3369650b87965fe9afb2
Devesh <rockdev1234560@gmail.com>
2021-06-14 01:32:16 +0530

    fix folder error of Footer and reduced padding, further changes possible

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,37 +1,25 @@
 import React from "react";
-import {
-Box,
-Container,
-Row,
-Column,
-FooterLink,
-Heading,
-} from "./FooterStyles";
+import { Box, Container, Row, FooterLink, Heading } from "./FooterStyles";
 
 const Footer = () => {
-return (
-	<Box>
-	<Container>
-		<Row>
-            <Heading>Contact Us</Heading>
-			<FooterLink href="admin@bitsg.acm.org">
-			<i className="far fa-envelope">
-				<span style={{ marginLeft: "10px" }}>
-				Email
-				</span>
-			</i>
-			</FooterLink>
-			<FooterLink href="https://www.linkedin.com/company/acm-bits-pilani-goa-chapter">
-			<i className="fab fa-linkedin-in">
-				<span style={{ marginLeft: "10px" }}>
-				LinkedIn
-				</span>
-			</i>
-			</FooterLink>
-
-		</Row>
-	</Container>
-	</Box>
-);
+  return (
+    <Box>
+      <Container>
+        <Row>
+          <Heading className="topFix">Contact Us</Heading>
+          <FooterLink href="admin@bitsg.acm.org">
+            <i className="far fa-envelope">
+              <span style={{ marginLeft: "10px" }}>Email</span>
+            </i>
+          </FooterLink>
+          <FooterLink href="https://www.linkedin.com/company/acm-bits-pilani-goa-chapter">
+            <i className="fab fa-linkedin-in">
+              <span style={{ marginLeft: "10px" }}>LinkedIn</span>
+            </i>
+          </FooterLink>
+        </Row>
+      </Container>
+    </Box>
+  );
 };
 export default Footer;
