685a61d0c76cfa07b71d8aaa13a5f31f015dd337
Devesh <rockdev1234560@gmail.com>
2021-06-15 10:22:24 +0530

    Footer Fixed and refactored

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const App = () => {
         <Route path="/resources" component={Resources} />
         <Route path="/team" component={Team} />
       </Switch>
+      <Footer />
       {/* <Footer /> */}
     </Router>
   );
